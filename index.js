1f8cd138c685d83835025d37be71a5d54ab56cb5
William Walsh <wmdwalsh0@gmail.com>
2021-07-05 22:36:51 -0400

    commit tree 34ba41ba519cdca126c36f58a53df89de9b68913

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(add,0);
+
+function add(accumulator, a){
+    return accumulator + a;
+}
+console.log(totalBatteries);
